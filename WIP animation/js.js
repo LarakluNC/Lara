6de1508bce550f750febd151333bab360e6471d0
Lara <lara@northerncomfort.co.uk>
2018-11-13 18:37:47 +0000

    adding counter to click animation

--- WIP animation/js.js
+++ WIP animation/js.js
@@ -12,39 +12,36 @@ jQuery(document).ready(function($) {
 	}, 700);	
 
 	$('.next').on('click', function(){ 
-		counter += 1;
-		$('.inner, .overflow, .overflow2').addClass('next-swipe');
+		$('.slide'+counter+' .inner, .slide'+counter+' .overflow, .slide'+counter+' .overflow2').addClass('next-swipe');
 		setTimeout(function(){ 
 				$('.wiper-next').addClass('next-wipe');
 			}, 500);
 			setTimeout(function(){ 
-				$('.slide1').addClass('hide-slide');
-				$('.inner, .overflow, .overflow2').removeClass('next-swipe');
+				$('.slide'+counter+' .inner, .slide'+counter+' .overflow, .slide'+counter+' .overflow2').removeClass('next-swipe');
 				$('.wiper-next').removeClass('next-wipe');
 				$(".allSlides").hide();	
 				$(".slide" + counter).css("z-index","2");
 			$(".slide" + counter).show();
-			}, 2000);	
-			console.log(counter);
+		}, 2000);	
+		counter += 1;
+		console.log(counter);
 	});
 
 	
 	$('.previous').on('click', function(){ 
-		counter -= 1;
-		$('.inner, .overflow, .overflow2').addClass('previous-swipe');
+		$('.slide'+counter+' .inner, .slide'+counter+' .overflow, .slide'+counter+' .overflow2').addClass('previous-swipe');
 		setTimeout(function(){ 
 			$('.wiper-previous').addClass('previous-wipe');
 			}, 500);
 			setTimeout(function(){ 
-				$('.slide1').addClass('hide-slide');
-				$('.inner, .overflow, .overflow2').removeClass('previous-swipe');
+				$('.slide'+counter+' .inner, .slide'+counter+' .overflow, .slide'+counter+' .overflow2').removeClass('previous-swipe');
 				$('.wiper-previous').removeClass('previous-wipe');
 				$(".allSlides").hide();	
 				$(".slide" + counter).css("z-index","2");
 			$(".slide" + counter).show();
-			}, 2000);	
-			console.log(counter);
-
+		}, 2000);	
+		counter -= 1;
+		console.log(counter);
 	});
 	
 });
